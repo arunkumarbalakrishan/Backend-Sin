cce00cbeada9b4d031085737e10f072d387bfc17
Arun Kumar <arunkumar0072002@gmail.com>
2026-03-23 19:44:36 +0530

    Delegates Explanation

--- Backend-Sin/Program.cs
+++ Backend-Sin/Program.cs
@@ -1,13 +1,51 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+#region Delegates 
 
-int a = 20;
-int b = 10;
+/*what is delegates ?
+    Delegates allow methods to be passed like variables.
 
-if (a > b)
+Why its exits ?
+
+    Pass behavior (logic) to another method.
+    Implement callbacks.
+    Event systems depend on delegates.*/
+//Real World  EG : 
+
+public class Program
 {
-    Console.WriteLine(true);
+    public delegate void LogHandler(string message);
+
+    public class OrderService
+    {
+        public void PlaceOrder(string item, LogHandler logger)
+        {
+            Console.WriteLine("Order placed for " + item);
+
+            logger("Order successfully placed for " + item);
+        }
+    }
+    public class Logger
+    {
+        public static void LogToConsole(string msg)
+        {
+            Console.WriteLine("Console Log: " + msg);
+        }
+
+        public static void LogToFile(string msg)
+        {
+            System.IO.File.AppendAllText("log.txt", msg + "\n");
+        }
+    }
+
+
+    public static void Main(string[] args)
+    {
+        OrderService service = new OrderService();
+
+        service.PlaceOrder("Laptop", Logger.LogToConsole);
+
+        service.PlaceOrder("Phone", Logger.LogToFile);
+    }
 }
-else
-{
-    Console.WriteLine(false);
-}
+
+#endregion
